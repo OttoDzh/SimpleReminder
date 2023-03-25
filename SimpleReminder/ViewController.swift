3dd76f0191e29c5482179b25998027c277ea399e
Otto <jiokotok@yandex.ru>
2023-03-25 12:17:34 +0300

    Initial Commit

---
+++ SimpleReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleReminder
+//
+//  Created by Otto Dzhandzhuliya on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
